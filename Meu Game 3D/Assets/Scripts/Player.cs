32623f921860a03b6f82a11e464ce91cef99f6c3
fernandacmpss <campos.fernanda@escolar.ifrn.edu.br>
2024-08-27 14:28:04 -0300

    Script do jogador

---
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int velocidade = 10;
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message:"START");
+        TryGetComponent(out rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(message:"UPDATE");
+        float h = Input.GetAxis("Horizontal"); //-1 esquerda,0 nada, 1 direita
+        float v = Input.GetAxis("Vertical"); // -1 pra tras, 0 nada, 1 pra frente
+
+        Vector3 direcao = new Vector3(x: h, y: 0, z: v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+
+
+
+        if (transform.position.y <= -10)
+        {
+            //jogador caiu
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+            
+    }
+}
