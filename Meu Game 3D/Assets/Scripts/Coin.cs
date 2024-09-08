989e933b4442a9de1629c231f07e287f3f52cf12
fernandacmpss <campos.fernanda@escolar.ifrn.edu.br>
2024-09-07 22:08:33 -0300

    passos finais

---
+++ Meu Game 3D/Assets/Scripts/Coin.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour 
+{
+    // Valor da moeda
+    public int coinValue = 1;
+
+    // Método que é chamado quando o jogador colide com a moeda
+    void OnTriggerEnter(Collider other)
+    {
+        if 
+        (other.CompareTag("Player")) // Verifica se o jogador colidiu com a moeda
+        {
+                // Destroi a moeda
+                 Destroy (gameObject);
+        }
+    }
+}
